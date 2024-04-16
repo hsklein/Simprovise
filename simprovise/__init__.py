3b9313803826c93230f9969494ba44e89280daa3
hsklein <167140054+hsklein@users.noreply.github.com>
2024-04-16 15:10:50 -0700

    Update __init__.py
    No need for this __init__.py to do anything, at least for now

--- simprovise/__init__.py
+++ simprovise/__init__.py
@@ -1,5 +1,2 @@
 #import matplotlib
 #matplotlib.rcParams['backend.qt4'] = 'PySide'
-#__all__ = ['Core', 'Animation', 'Configuration', 'RunControl', 'UI', 'Database', 'Models']
-
-from simprovise.simulation import Simulation, SimulationResult
