106af406baf996220a4a9e0a7d56e623793738eb
hsklein <167140054+hsklein@users.noreply.github.com>
2024-05-11 14:39:26 -0700

    Move resource acquire/release from SimTransaction to SimProcess
    SimProcess is intended to be the only transaction subclass that can acquire SimResources. Facilitates the introduction of other agent classes (possibly monitors and/or timers) that use transactions without acquiring resources

--- simprovise/core/process.py
+++ simprovise/core/process.py
@@ -7,15 +7,43 @@
 #===============================================================================
 __all__ = ['SimProcess']
 
-from simprovise.core.transaction import SimTransaction
+from simprovise.core.transaction import SimTransaction, SimTransactionInterruptEvent
 from simprovise.core.simelement import SimClassElement 
+from simprovise.core.agent import SimMsgType
 from simprovise.core.apidoc import apidoc, apidocskip
 
-from simprovise.core import SimEntity, SimCounter, SimTime, SimUnweightedDataCollector
-from simprovise.core import SimLogging, SimError
+from simprovise.core import (SimEntity, SimCounter, SimTime, SimClock,
+                             SimUnweightedDataCollector)
+from simprovise.core import SimLogging, SimError, SimTimeOutException
 logger = SimLogging.get_logger(__name__)
 
 _ERROR_NAME = "SimProcessError"
+
+
+class SimTimeOutEvent(SimTransactionInterruptEvent):
+    """
+    Timeout a resource acquisition request. As with the base class
+    Interrupt event, we rely on the higher priority of Resume events
+    to ensure that if the assignment/resume and timeout occur at the
+    same simulated time, the resume will be processed first and
+    deregister this event.
+    """
+    __slots__ = ('assignmentAgent', 'requestMsg')
+    def __init__(self, transaction, agent, msg, timeout=0):
+        super().__init__(transaction, SimTimeOutException('timeout'),
+                         SimClock.now() + timeout)
+        self.assignmentAgent = agent
+        self.requestMsg = msg
+        
+    def process_impl(self):
+        """
+        Event processing wakes up the timed-out resource request via
+        base class implementation, and  then asks the assignment agent
+        to remove the assignment request from the queue.
+        """
+        super().process_impl()
+        self.assignmentAgent.remove_request_message(self.requestMsg)
+
         
 @apidoc
 class SimProcess(SimTransaction):
@@ -23,8 +51,7 @@ class SimProcess(SimTransaction):
     SimProcess is a subclass of SimTransaction, where entities are the agents.
     As such it is the base class for all simulation processes.
     """
-    __slots__ = ('__executing', '__entity', '__element',
-                 '__resourceAssignments')
+    __slots__ = ('__executing', '__entity', '__element', '__resource_assignments')
     
     elements = {}
     
@@ -84,7 +111,7 @@ class SimProcess(SimTransaction):
         self.__entity = None
         self.__element = self.__class__.element
         assert self.__element, "No element exists for process class"
-        self.__resourceAssignments = []
+        self.__resource_assignments = []
 
     @property
     def element(self):
@@ -112,7 +139,229 @@ class SimProcess(SimTransaction):
     def entity(self, value):
         assert isinstance(value, SimEntity), "Attempt to set SimProcess.entity to an object of type " + str(type(value))
         self._agent = value
+        
+    @apidocskip
+    def execute(self):
+        """
+        Execute the process (synchronously). Should not be called by client code.
 
+        Invoke the base class execute(), (which in turn invokes run()); when
+        complete, make sure there are no resource assignments that have not
+        been released. (Process execution should not end with unreleased
+        resources.)
+        """
+        super().execute()
+        hasUnreleasedAssignments = any([True for assg in self.__resource_assignments if assg.count > 0])
+        if hasUnreleasedAssignments:
+            unreleasedAssignments = [assg for assg in self.__resource_assignments if assg.count > 0]
+            msg = "Transaction {0} run() error.  All resources must be released prior to the end of process execution (run()).  The following resource assignments have not been released: {1}"
+            raise SimError(_ERROR_NAME, msg, self, unreleasedAssignments)
+        
+        
+    def acquire(self, resource, numrequested=1, *, timeout=None):
+        """
+        Acquires a specified resource (or resources) on behalf of this
+        transaction, blocking until either the resource(s) are acquired,
+        or the request times out.
+        
+        If the resource(s) are acquired, a resource assignment is returned.
+        If `acquire` times out, a :class:`~.simexception.SimTimeOutException`
+        is raised. If the timeout parameter value is `None`, `acquire` will
+        not time out. If the timeout is zero, the request times out if the
+        requested resource(s) are not immediately available and assigned.
+        
+        Note that default behavior typically guarantees that all of
+        the requested resources will be acquired; client code can
+        customize this behavior, possibly providing some but not all
+        of the requested resource if the number requested is > 1.
+
+        :param resource:     The resource object to be acquired
+        :type resource:      :class:`~.resource.SimResource`
+
+        :param numrequested: The number of resources (subresources) to acquire,
+                             Can be more than 1 if resource has capacity greater
+                             than 1. Defaults to 1
+        :type numrequested:   `int`
+
+        :param timeout:      If not None, the length of simulated time
+                             after which the request times out. If None,
+                             the request never times out (unless interrupted by
+                             the assignment agent) Defaults to `None`.
+        :type timeout:       :class:`~.simtime.SimTime` or `None`.
+        
+        :raises:             :class:`~.simexception.SimTimeOutException`
+                             Raised if acquire() request time out.
+        
+        :return:             Assignment object that specifies assigned resource(s)
+        :rtype:              :class:`~.resource.SimResourceAssignment`
+        
+        """
+        assert resource.assignment_agent, "Resource has no assignment agent!"
+
+        if numrequested <= 0 or int(numrequested) != numrequested:
+            errorMsg = "Resource acquire() number requested ({0}) must be an integer greater than zero"
+            raise SimError(_ACQUIRE_ERROR, errorMsg, numrequested)
+
+        # Send a resource request message to the desired resource's assignment
+        # agent (which may or may not be the resource itself)
+        assignmentAgent = resource.assignment_agent
+        msgData = (self, numrequested, resource)
+        return self._acquire_impl(assignmentAgent, msgData, timeout)
+
+    def acquire_from(self, agent, rsrcClass, numrequested=1, *, timeout=None):
+        """
+        Acquire a resource (or resources) of a specified class that is
+        managed by a specified assignment agent, returning a resource
+        assignment. Blocks until the resource(s) are assigned. Typically
+        used when acquiring resource(s) managed by a
+        :class:`.resource.SimResourcePool`.
+        
+        As with :meth:`~SimTransaction.acquire`, the request can time out
+        if a `timeout` parameter value other than `None` is supplied. If
+        the request times out, a :class:`~.simexception.SimTimeOutException`
+        is raised. If the timeout is zero, the request times out if the
+        requested resource(s) are not immediately available and assigned.
+        
+        Note that default behavior typically guarantees that all of
+        the requested resources will be acquired; client code can
+        customize this behavior, possibly providing some but not all
+        of the requested resource if the number requested is > 1.
+
+        :param agent:        The assignment agent managing the desired
+                             resource(s)
+        :type agent:         :class:`~.resource.SimResourceAssignmentAgent`
+
+        :param rsrcClass:    The (Python) class of resource object to be
+                             acquired. May be a base class, if a heterogenous
+                             set of resources can meet the request.
+        :type rsrcClass:     `class` derived from :class:`~.resource.SimResource`
+
+        :param numrequested: The number of resources or subresources to acquire.
+                             Can be more than 1 if the agent manages multiple
+                             resources (or a resource with capacity > 1).
+                             Defaults to 1
+        :type numrequested:   `int`
+
+        :param timeout:      If not None, the length of simulated time
+                             after which the request times out. If None,
+                             the request never times out (unless interrupted by
+                             the assignment agent) Defaults to `None`.
+        :type timeout:       :class:`~.simtime.SimTime` or `None`.
+        
+        :raises:             :class:`~.simexception.SimTimeOutException`
+                             Raised if acquire() request time out.
+        
+        :return:             Assignment object that specifies assigned resource(s)
+        :rtype:              :class:`~.resource.SimResourceAssignment`
+        
+        """
+        assert isinstance(rsrcClass, type), "acquireFrom() rsrcClass parameter is not a class"
+        assert agent, "Null agent passed to acquireFrom()"
+
+        if numrequested <= 0 or int(numrequested) != numrequested:
+            errorMsg = "Resource acquire() number requested ({0}) must be an integer greater than zero"
+            raise SimError(_ACQUIRE_ERROR, errorMsg, numrequested)
+
+        msgData = (self, numrequested, rsrcClass)
+        return self._acquire_impl(agent, msgData, timeout)
+
+    def _acquire_impl(self, assignmentAgent, msgData, timeout):
+        """
+        Implements the bulk of the resource acquisition that is common to both
+        acquire() and acquire_from()
+        """
+        assert self.agent, "Process calling acquire() has no agent"
+        assert self.is_executing, "Resources can only be acquired by executing processes"
+        
+        if timeout is not None:
+            if not isinstance(timeout, SimTime):
+                # let this raise if timeout cannot be converted to a SimTime
+                timeout = SimTime(timeout)
+            if timeout < 0:
+                msg = "Process {0} invoked a resource acquire with timeout < 0: {1}"
+                raise SimError(_ERROR_NAME, msg, self, timeout)
+         
+
+        msgType = SimMsgType.RSRC_REQUEST
+        msg, responses = self.agent.send_message(assignmentAgent,
+                                                msgType, msgData)
+        if responses:
+            # If the assignment agent responded immediately, grab that response
+            # (which should just be a resource assignment message)
+            assert len(responses) == 1, "Resource Request message expects zero or one responses"
+            response = responses[0]
+            assert response.msgType == SimMsgType.RSRC_ASSIGNMENT, "Response to Resource request was not a resource assignment"
+        else:
+            # If the assignment agent did not respond immediately (presumably
+            # because the requested resource is not available), wait for a
+            # response. Schedule a timeout that will be executed if the resource
+            # request is not fulfilled first.
+            if timeout is not None:
+                timeoutEvent = SimTimeOutEvent(self, assignmentAgent, msg, timeout)
+                timeoutEvent.register()
+            response = self.wait_for_response(msg)
+            assert response, "No response from waitForRespoonse"
+            assert response.msgType == SimMsgType.RSRC_ASSIGNMENT, "Response to Resource request was not a resource assignment"
+
+        # Extract the assignment from the response message and register it
+        assignment = response.msgData
+        assert assignment.transaction is self, "Resource assignment does not specify this transaction"
+        self.__resource_assignments.append(assignment)
+
+        # Finally, return the assignment
+        return assignment
+
+    def release(self, rsrcAssignment, releaseSpec=None):
+        """
+        Release some or all of the resources in the passed assignment (the
+        assignment should be to this transaction). If a subset of the
+        assignment's resources are to be released, it/they may be specified
+        in one of three ways:
+
+            1. As an iterable to the resource objects to be released
+            2. As a single resource object
+            3. As a number n, specifying the release of the first n resources in
+               the passed assignment.
+
+        The default release spec value of None indicates that all resources
+        in the assignment are to be released.
+        
+        The release is actually accomplished by sending a RSRC_RELEASE message
+        to the resource asssignment agent, which also subtracts the released
+        resources from the resource assignment object.
+
+        :param rsrcAssignment: The resource assignment that is to be fully or
+                               partially released.
+        :type rsrcAssignment:  :class:`~.resource.SimResourceAssignment`
+
+        :param releaseSpec:    Specification of resources within the assignment
+                               to release (as described above) or None, to
+                               release all resources in rsrcAssignment.
+        :type releaseSpec:     See above.
+ 
+        """
+        assert self.agent, "Transaction calling release() has no agent"
+        assert self.is_executing, "Resources can only be released by executing transactions"
+        assert rsrcAssignment, "Null assignment passed to release()"
+        assert rsrcAssignment.assignment_agent, "Resource assignment has no agent"
+        assert rsrcAssignment.transaction == self, "Resource assignment transaction is not this transaction"
+
+        assignmentAgent = rsrcAssignment.assignment_agent
+        msgType = SimMsgType.RSRC_RELEASE
+        msgData = (rsrcAssignment, releaseSpec)
+        self.agent.send_message(assignmentAgent, msgType, msgData)
+        if rsrcAssignment.count == 0:
+            self.__resource_assignments.remove(rsrcAssignment)
+
+    def resource_assignments(self):
+        """
+        Returns a list of current resource assignments
+        (:class:`~.resource.SimResourceAssignment`) for the transaction;
+        any assignment with a resource count of zero is ignored
+        (i.e., is not included in the returned list).
+        """
+        return [assg for assg in self.__resource_assignments if assg.count > 0]
+        
     def __str__(self):
         return self.__class__.__name__
     
