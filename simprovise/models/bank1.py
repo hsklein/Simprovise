7306c9ad296b7f8e8b56e2137783c28125d35f2a
hsklein <167140054+hsklein@users.noreply.github.com>
2024-06-30 13:59:20 -0700

    Tutorial model updates

--- simprovise/models/bank1.py
+++ simprovise/models/bank1.py
@@ -5,8 +5,9 @@
 #
 # Defines one iteration of the bank demo model.
 #===============================================================================
-import sys
+import sys, os
 from enum import Enum
+#print("*********importing bank1****** pid:", os.getpid())
 from simprovise.core import simtime, simtrace
 from simprovise.core.simtime import SimTime, Unit as tu
 from simprovise.core.simrandom import SimDistribution
@@ -19,153 +20,83 @@ from simprovise.modeling import (SimEntity, SimEntitySource, SimEntitySink,
 from simprovise.simulation import Simulation
 from simprovise.models.queuing_theory_calc import theory_results
 
+            
+class Teller(SimSimpleResource):
+    """
+    A set of bank teller resources (capacity > 1 represents multiple
+    tellers)
+    """
+
 class Customer(SimEntity):
     """
     Base class for bank customer entities
     """
-
-class RegularCustomer(Customer):
-    """
-    Regular (not merchant) bank customer
-    """
-
-class MerchantCustomer(Customer):
-    """
-    Merchant bank customer
-    """
-    
-class Teller(SimSimpleResource):
-    """
-    Base class for teller resources
-    """
-
-class RegularTeller(Teller):
-    """
-    A teller primarily for regular customers
-    """
-    def __str__(self):
-        return "RegularTeller"
-
-class MerchantTeller(Teller):
-    """
-    A teller primarily for merchant customers
-    """
-    def __str__(self):
-        return "MerchantTeller"
     
 class Bank(SimLocation):
     """
     A simLocation that encapsulates all of the objects (resources,
     queues and locations) that comprise a bank.
     """
-    __slots__ = ('teller_counter', 'regular_teller', 'merchant_teller',
-                 'teller_pool', 'regular_queue', 'merchant_queue')
-    
-    def __init__(self, name="Bank", nRegularTellers=4, nMerchantTellers=1):
+    def __init__(self, name="Bank", nTellers=1):
         super().__init__(name)
         self.teller_counter = SimLocation("TellerCounter", self)
-        self.regular_teller = RegularTeller("RegularTeller",
-                                            self.teller_counter,
-                                            capacity = nRegularTellers)
+        self.teller = Teller("Teller", self.teller_counter, capacity = nTellers)       
+        self.customer_queue = SimQueue("CustomerQueue", self)
         
-        self.merchant_teller = MerchantTeller("MerchantTeller",
-                                              self.teller_counter,
-                                              capacity = nMerchantTellers)
         
-        self.teller_pool = SimResourcePool(self.regular_teller, 
-                                           self.merchant_teller)
-        
-        self.regular_queue = SimQueue("RegularQueue", self)
-        self.merchant_queue = SimQueue("MerchantQueue", self)
+# The bank model consists of a (customer) source, a (customer) sink, and
+# a bank with a specified number of tellers.
+source = SimEntitySource("Source")
+sink = SimEntitySink("Sink")
+bank = Bank(nTellers=4)
 
+# Customer interarrival times and service times are both exponentially
+# distributed. Set distribution means and create generators for psuedo-
+# random samples.
+mean_interarrival_time = SimTime(1, tu.MINUTES)
+mean_service_time = SimTime(3, tu.MINUTES) 
+servicetime_generator = SimDistribution.exponential(mean_service_time)
+interarrivaltime_generator = SimDistribution.exponential(mean_interarrival_time)
 
 class BankTransaction(SimProcess):
     """
-    Base class for simulated bank transaction classes
+    Class for simulated bank transaction classes
     """        
-
-class RegularTransaction(BankTransaction):
-    """
-    Represents a "regular" transaction by a "regular" (non-merchant)
-    customer.
-    """
-    mean_interarrival_time = SimTime(1, tu.MINUTES)
-    mean_service_time = SimTime(3, tu.MINUTES) 
-    st_generator = SimDistribution.exponential(mean_service_time)
-
     def run(self):
-        bank = SimModel.model().get_static_object("Bank")
-        sink = SimModel.model().get_static_object("Sink")
-        service_time = next(RegularTransaction.st_generator)
+        service_time = next(servicetime_generator)
         customer = self.entity
-        customer.move_to(bank.regular_queue)
-        with self.acquire_from(bank.teller_pool, RegularTeller) as teller_assignment:
-            teller = teller_assignment.resource
+        customer.move_to(bank.customer_queue)
+        with self.acquire(bank.teller) as teller_assignment:
             customer.move_to(bank.teller_counter)
             self.wait_for(service_time)
         customer.move_to(sink)
+        
+# Dreate the (customer) entity generators for the model's entity source.
+source.add_entity_generator(Customer, BankTransaction, interarrivaltime_generator)
 
-class MerchantTransaction(BankTransaction):
-    """
-    Represents a merchant transaction (by a merchant customer)
-    """
-    mean_interarrival_time = SimTime(6, tu.MINUTES)
-    mean_service_time = SimTime(4, tu.MINUTES)
-    st_generator = SimDistribution.exponential(mean_service_time)
-
-    def run(self):
-        bank = SimModel.model().get_static_object("Bank")
-        sink = SimModel.model().get_static_object("Sink")
-        service_time = next(MerchantTransaction.st_generator)
-        customer = self.entity
-        customer.move_to(bank.merchant_queue)
-        with self.acquire_from(bank.teller_pool, MerchantTeller) as teller_assignment:
-            teller = teller_assignment.resource
-            customer.move_to(bank.teller_counter)
-            self.wait_for(service_time)
-        customer.move_to(sink)
-
-# The bank model consists of a (customer) source, a (customer) sink, and
-# a bank.
-source = SimEntitySource("Source")
-sink = SimEntitySink("Sink")
-bank = Bank(name="Bank", nRegularTellers=4, nMerchantTellers=1)
-
-# Define and create the (customer) entity generators for the model's entity
-# source.
-dist_reg = SimDistribution.exponential(RegularTransaction.mean_interarrival_time) 
-dist_merch = SimDistribution.exponential(MerchantTransaction.mean_interarrival_time) 
-source.add_entity_generator(RegularCustomer, RegularTransaction, dist_reg)
-source.add_entity_generator(MerchantCustomer, MerchantTransaction, dist_merch)
 
 # Set up trace output
-#simtrace.set_tracetype(simtrace.TraceType.TABLE)
-#simtrace.set_trace_stdout()
-simtrace.add_trace_column(bank.regular_queue, 'current_population', 'Regular Queue')
-simtrace.add_trace_column(bank.merchant_queue, 'current_population', 'Merchant Queue')
-simtrace.add_trace_column(bank.regular_teller, 'available', 'RegularTellers: available')
-simtrace.add_trace_column(bank.merchant_teller, 'available', 'MerchantTellers: available')
+simtrace.add_trace_column(bank.teller, 'available', 'Tellers: available')
+#simtrace.add_trace_column(bank.merchant_teller, 'available', 'MerchantTellers: available')
 
 
 if __name__ == '__main__':
-    print("================ main=================")
-    print("debug:", __debug__)
+    #print("================ main=================")
     warmupLength = SimTime(100, tu.MINUTES)
-    batchLength = SimTime(600, tu.MINUTES)
-    warmupLength = SimTime(10, tu.MINUTES)
-    batchLength = SimTime(60, tu.MINUTES)
-    #bl = SimTime(10000)
-    print("Running single execution...")
-    with Simulation.execute(warmupLength, batchLength, 10,
-                            outputpath=None, overwrite=False) as simResult:
+    batchLength = SimTime(1000, tu.MINUTES)
+    nbatches = 10
+    nruns = 10
+    print("Running {0} replications...".format(nruns))
+    with Simulation.execute(warmupLength, batchLength, nbatches) as simResult:
         simResult.print_summary()
+        
+    #with Simulation.replicate(None, warmupLength, batchLength, nbatches,
+                              #fromRun=1, toRun=nruns) as simResult:
+        #simResult.print_summary()
+    
  
-    print("\nRegular Customers")
-    theory_results(RegularTransaction.mean_interarrival_time.to_scalar(),
-                   RegularTransaction.mean_service_time.to_scalar(),
-                   bank.regular_teller.capacity)
+    print("\nM/M/{0} theory result:".format(bank.teller.capacity))
+    theory_results(mean_interarrival_time.to_scalar(),
+                  mean_service_time.to_scalar(),
+                   bank.teller.capacity)
  
-    print("\nMerchant Customers")
-    theory_results(MerchantTransaction.mean_interarrival_time.to_scalar(),
-                   MerchantTransaction.mean_service_time.to_scalar(),
-                   bank.merchant_teller.capacity)
