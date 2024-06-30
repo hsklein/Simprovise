d5fc568a593c4288cf6532124dedf49c580f3ffb
hsklein <167140054+hsklein@users.noreply.github.com>
2024-06-30 13:58:46 -0700

    Tutorial model updates

--- simprovise/models/bank2.py
+++ simprovise/models/bank2.py
@@ -17,6 +17,7 @@ from simprovise.modeling import (SimEntity, SimEntitySource, SimEntitySink,
                                  SimResourcePool, SimSimpleResource, SimQueue)
 
 from simprovise.simulation import Simulation
+from simprovise.models.queuing_theory_calc import theory_results
 
 class Customer(SimEntity):
     """
@@ -25,13 +26,25 @@ class Customer(SimEntity):
 
 class RegularCustomer(Customer):
     """
-    Regular (not merchant) bank customer
+    Regular (not merchant) bank customer.
+    
+    Defines a priority function used to prioritize merchant
+    customers over regular customers when competing for a
+    merchant teller.
     """
+    def priority(self):
+        return 2
 
 class MerchantCustomer(Customer):
     """
     Merchant bank customer
+    
+    Defines a priority function used to prioritize merchant
+    customers over regular customers when competing for a
+    merchant teller.
     """
+    def priority(self):
+        return 1
     
 class Teller(SimSimpleResource):
     """
@@ -52,62 +65,21 @@ class MerchantTeller(Teller):
     def __str__(self):
         return "MerchantTeller"
     
-class SimTellerPool(SimResourcePool):
-    """
-    """
-    def _queued_regular_requests(self):
-        """
-        """
-        return [request for request in self.queued_resource_requests()
-                if isinstance(request.entity, RegularCustomer)]
-        
-    def _queued_merchant_requests(self):
-        """
-        """
-        return [request for request in self.queued_resource_requests()
-                if isinstance(request.entity, MerchantCustomer)]    
-        
-    def process_queued_requests(self, throughRequest=None):
-        """
-        """
-        # Assign merchant customers to merchant tellers until we run
-        # out of one or the other
-        available_tellers = self.available_resources(MerchantTeller)
-        for request in self._queued_merchant_requests():
-            if available_tellers:
-                teller = available_tellers.pop()
-                request.assign_resource(teller)
-            else:
-                break
-          
-        # Do the same for regular customers and tellers
-        available_tellers = self.available_resources(RegularTeller)
-        for request in self._queued_regular_requests():
-            if available_tellers:
-                teller = available_tellers.pop()
-                request.assign_resource(teller)
-            else:
-                break
-            
-        # If there are unassigned tellers of any type left over and any  
-        # customers remaining, assign customers to any type of teller
-        available_tellers = self.available_resources(Teller)
-        for request in self.queued_resource_requests():
-            if available_tellers:
-                teller = available_tellers.pop()
-                request.assign_resource(teller)
-            else:
-                break
-                
-              
 class Bank(SimLocation):
     """
-    A SimLocation that encapsulates all of the objects (resources,
+    A simLocation that encapsulates all of the objects (resources,
     queues and locations) that comprise a bank.
     """
-    __slots__ = ('teller_counter', 'regular_teller', 'merchant_teller',
-                 'teller_pool', 'regular_queue', 'merchant_queue')
-    
+    @staticmethod
+    def get_priority(teller_request):
+        """
+        The priority function to be registered with the teller
+        resource pool. It returns the value of the priority()
+        function implemented by the requests 
+        """
+        return teller_request.entity.priority()
+        
+        
     def __init__(self, name="Bank", nRegularTellers=4, nMerchantTellers=1):
         super().__init__(name)
         self.teller_counter = SimLocation("TellerCounter", self)
@@ -119,8 +91,11 @@ class Bank(SimLocation):
                                               self.teller_counter,
                                               capacity = nMerchantTellers)
         
-        self.teller_pool = SimTellerPool(self.regular_teller, 
-                                         self.merchant_teller)
+        self.teller_pool = SimResourcePool(self.regular_teller, 
+                                           self.merchant_teller)
+        
+        # Specify a resource request priority function for the teller pool
+        self.teller_pool.request_priority_func = Bank.get_priority
         
         self.regular_queue = SimQueue("RegularQueue", self)
         self.merchant_queue = SimQueue("MerchantQueue", self)
@@ -137,7 +112,7 @@ class RegularTransaction(BankTransaction):
     customer.
     """
     mean_interarrival_time = SimTime(1, tu.MINUTES)
-    mean_service_time = SimTime(3, tu.MINUTES) 
+    mean_service_time = SimTime(2, tu.MINUTES) 
     st_generator = SimDistribution.exponential(mean_service_time)
 
     def run(self):
@@ -157,7 +132,7 @@ class MerchantTransaction(BankTransaction):
     Represents a merchant transaction (by a merchant customer)
     """
     mean_interarrival_time = SimTime(6, tu.MINUTES)
-    mean_service_time = SimTime(4, tu.MINUTES)
+    mean_service_time = SimTime(3, tu.MINUTES)
     st_generator = SimDistribution.exponential(mean_service_time)
 
     def run(self):
@@ -166,7 +141,7 @@ class MerchantTransaction(BankTransaction):
         service_time = next(MerchantTransaction.st_generator)
         customer = self.entity
         customer.move_to(bank.merchant_queue)
-        with self.acquire_from(bank.teller_pool, Teller) as teller_assignment:
+        with self.acquire_from(bank.teller_pool, MerchantTeller) as teller_assignment:
             teller = teller_assignment.resource
             customer.move_to(bank.teller_counter)
             self.wait_for(service_time)
@@ -176,29 +151,30 @@ class MerchantTransaction(BankTransaction):
 # a bank.
 source = SimEntitySource("Source")
 sink = SimEntitySink("Sink")
-bank = Bank(name="Bank", nRegularTellers=4, nMerchantTellers=1)
+bank = Bank(name="Bank", nRegularTellers=2, nMerchantTellers=1)
 
 # Define and create the (customer) entity generators for the model's entity
-# source - one generator for regular customer entities, one for merchant customers.
+# source.
 dist_reg = SimDistribution.exponential(RegularTransaction.mean_interarrival_time) 
 dist_merch = SimDistribution.exponential(MerchantTransaction.mean_interarrival_time) 
 source.add_entity_generator(RegularCustomer, RegularTransaction, dist_reg)
 source.add_entity_generator(MerchantCustomer, MerchantTransaction, dist_merch)
 
-# Set up trace output columns
-simtrace.add_trace_column(bank.regular_queue, 'current_population', 'Regular Queue')
-simtrace.add_trace_column(bank.merchant_queue, 'current_population', 'Merchant Queue')
-simtrace.add_trace_column(bank.regular_teller, 'available', 'RegularTellers: available')
-simtrace.add_trace_column(bank.merchant_teller, 'available', 'MerchantTellers: available')
+# Set up trace output
+#simtrace.set_tracetype(simtrace.TraceType.TABLE)
+#simtrace.set_trace_stdout()
+simtrace.add_trace_column(bank.regular_queue, 'current_population', 'Reg Queue')
+simtrace.add_trace_column(bank.merchant_queue, 'current_population', 'Merch Queue')
+simtrace.add_trace_column(bank.regular_teller, 'available', 'Reg: avail')
+simtrace.add_trace_column(bank.merchant_teller, 'available', 'Merch: avail')
 
 
 if __name__ == '__main__':
     print("================ main=================")
-    print("debug:", __debug__)
     warmupLength = SimTime(100, tu.MINUTES)
     batchLength = SimTime(600, tu.MINUTES)
-    warmupLength = SimTime(10, tu.MINUTES)
-    batchLength = SimTime(60, tu.MINUTES)
+    #warmupLength = SimTime(10, tu.MINUTES)
+    #batchLength = SimTime(60, tu.MINUTES)
     #bl = SimTime(10000)
     print("Running single execution...")
     with Simulation.execute(warmupLength, batchLength, 10,
